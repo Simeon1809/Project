3d09d704ccdaedc86914b67a869fb0ed34400e01
Simeon Ayara <simeondigital@gmail.com>
2021-12-13 21:19:28 +0100

    Updated the error page

--- project/src/Route/About.js
+++ project/src/Route/About.js
@@ -1,6 +1,6 @@
 import React, {} from 'react';
 import '../index.css';
-import about from './about.jpeg'
+import about from '../images/about.jpeg'
 
 const About = () =>
 {
@@ -8,11 +8,11 @@ const About = () =>
         <section className='hero'>
             <div className="about">
             <div>
-         <img src={about} alt="about image"/>
+         <img src={about} alt="about"/>
             </div>
             <div>
                 <h2>HELLO GUYS</h2>
-                <p>My name is simeon, am a full-stack software developer based in Lagos, Nigeria,i have a Strong background in design and integration with intuitive problem-solving skills.I have experience in the energy sector having worked in the poweer sector for over 5 years. Proficient in JAVASCRIPT, C#, and SQL. Passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions. Looking to start the career as an entry-level software engineer with a reputed firm driven by technology.</p>
+                <p>My name is simeon, am a full-stack software developer based in Lagos, Nigeria,i have a Strong background in design and integration with intuitive problem-solving skills.I have experience in the energy sector having worked in the poweer sector for over 5 years. Proficient in JAVASCRIPT, C#, and SQL. Passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions. Looking to start the career as an entry-level software engineer with a reputed firm driven by technology...</p>
             </div>
         </div>
 
