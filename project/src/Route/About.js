d0c97711bfec51cc28da12d4336021bb96f30711
Simeon1809 <81197471+Simeon1809@users.noreply.github.com>
2023-01-29 09:39:52 +0100

    Update About.js

--- project/src/Route/About.js
+++ project/src/Route/About.js
@@ -15,7 +15,6 @@ const About = () =>
                 <p>My name is simeon, am a full-stack software developer based in Lagos, Nigeria,i have a Strong background in design and integration with intuitive problem-solving skills.I have experience in the energy sector having worked in the poweer sector for over 5 years. Proficient in JAVASCRIPT, C#, and SQL. Passionate about implementing and launching new projects. Ability to translate business requirements into technical solutions. Looking to start the career as an entry-level software engineer with a reputed firm driven by technology....</p>
             </div>
         </div>
-
         </section>
     )
 }
